40feb126544f12207fa248824c5d5e3a35a03f5f
zDefcon <antholozadarequelme@gmail.com>
2026-04-09 03:23:03 -0500

    Agrega implementación del método sumaRecursiva
    Se implementa el método sumaRecursiva para calcular la suma de los números enteros desde 1 hasta n de forma recursiva. También se añade una llamada a este método en el método main para mostrar el resultado en la consola.

--- src/Ejemplo_Suma.java
+++ src/Ejemplo_Suma.java
@@ -1,9 +1,20 @@
-﻿public class Ejemplo_Suma {
+public class Ejemplo_Suma {
     public static void main(String[] args) {
-
+        int n = 10;
+        int res = sumaRecursiva(n);
+        System.out.println("La suma total es: " + res);
     }
 
-    public static int sumaRecursiva(int num){
+    public static int sumaRecursiva(int n){
+        int res;
 
+        if(n == 1){
+            return 1;
+        }
+        else{
+            res = n + sumaRecursiva(n -1);
+        }
+
+        return res;
     }
 }
