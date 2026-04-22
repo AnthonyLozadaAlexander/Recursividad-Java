9549b6b695e442a382bb7b06f1e7dea009878806
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 22:46:17 -0500

    Agrega clase EFactorial con método para calcular factorial

---
+++ src/Ejercicios/EFactorial.java
@@ -0,0 +1,16 @@
+﻿package Ejercicios;
+
+public class EFactorial {
+    static void main(String[] args) {
+        int num = 5; int res = calcularFactorial(num);
+        System.out.println("El Factorial De " + num + "! : " + res);
+    }
+
+    static int calcularFactorial(int num){
+        if(num == 1 || num == 0){
+            return 1;
+        }else{
+            return num*(calcularFactorial(num-1));
+        }
+    }
+}
