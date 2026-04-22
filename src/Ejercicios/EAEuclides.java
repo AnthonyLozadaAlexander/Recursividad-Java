20d23db5988f11f26ab4f6f8bf9a30fcdd6b4422
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 23:06:42 -0500

    Agrega método para calcular el MCD en EAEuclides
    Se implementa el método calcularMCD que utiliza recursión para determinar el máximo común divisor de dos números enteros. Este cambio mejora la funcionalidad de la clase EAEuclides al proporcionar una herramienta matemática útil.

--- src/Ejercicios/EAEuclides.java
+++ src/Ejercicios/EAEuclides.java
@@ -4,4 +4,12 @@ class EAEuclides{
     static void main(String[] args) {
 
     }
+
+    static int calcularMCD(int a, int b){
+        if(b == 0){
+            return Math.abs(a);
+        }
+
+        return calcularMCD(b, a % b);
+    }
 }
