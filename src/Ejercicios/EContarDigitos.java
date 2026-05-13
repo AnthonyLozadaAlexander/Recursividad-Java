a0ee82332052eee675021a5089df3dba8c121439
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-12 19:56:07 -0500

    Agrega clase EContarDigitos con método para contar dígitos
    Esta clase permite al usuario ingresar un número entero positivo y calcula la cantidad de dígitos que contiene. Se incluye un temporizador para medir el tiempo de ejecución del proceso.

---
+++ src/Ejercicios/EContarDigitos.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class EContarDigitos {
+    static void main(String[] args) {
+        Long start = System.nanoTime();
+        Scanner input = new Scanner(System.in);
+        int n = 0, digitos = 0;
+        System.out.println("--------------------------");
+        System.out.println("    CONTADOR DE DIGITOS   ");
+        System.out.println("--------------------------");
+        System.out.print("Ingrese el numero entero Positivo: "); n = input.nextInt();
+        input.nextLine();
+        digitos = ContarDigitos(n);
+        System.out.println("Numero Ingresado: " + n);
+        System.out.println("Digitos: " + digitos);
+        System.out.println("-------------------------");
+        Long end = System.nanoTime();
+        System.out.println("Tiempo Total Ejecucion: " + (end - start) + " nanosegundos");
+    }
+
+    public static int ContarDigitos(int n){
+        if(n < 10){
+            return 1;
+        }
+
+        return 1 + ContarDigitos(n / 10);
+    }
+}
