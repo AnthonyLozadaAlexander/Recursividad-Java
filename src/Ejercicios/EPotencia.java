0b01c1bba74edc43358e14b4623f4a4de1363138
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-12 19:25:31 -0500

    Agrega clase EPotencia con método para calcular potencias
    Se implementa la clase EPotencia que permite al usuario ingresar una base y un exponente, y calcula la potencia utilizando recursión. Esto proporciona una nueva funcionalidad para realizar cálculos de potencias en el proyecto.

---
+++ src/Ejercicios/EPotencia.java
@@ -0,0 +1,21 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class EPotencia {
+    static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int a = 0, n = 0;
+        System.out.print("Ingrese La Base: "); a = input.nextInt(); input.nextLine();
+        System.out.print("Ingrese El Exponente: "); n = input.nextInt(); input.nextLine();
+        int res = PotenciaR(a, n);
+        System.out.println(a+"^"+n+ " : " + res);
+    }
+
+    public static int PotenciaR(int a, int n){
+       if(n == 0){
+           return 1;
+       }
+       return a*PotenciaR(a,n-1);
+    }
+}
