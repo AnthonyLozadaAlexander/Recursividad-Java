f4c7f293ca9ba6efdeab0e6dbc8ce74ea0b0e1c2
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-12 20:56:38 -0500

    Agrega método para calcular la secuencia de Fibonacci
    Implementa el método factorialR en la clase EFibonacci para calcular la secuencia de Fibonacci de manera recursiva. También se agrega un método main que permite al usuario ingresar un límite y muestra la secuencia correspondiente.

--- src/Ejercicios/EFibonacci.java
+++ src/Ejercicios/EFibonacci.java
@@ -1,7 +1,32 @@
-﻿package Ejercicios;
+package Ejercicios;
+
+import java.util.Scanner;
 
 public class EFibonacci {
-    static void main(String[] args) {
-
+    public static int factorialR(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        return factorialR(n-1) + factorialR(n - 2);
     }
+
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int limite = 0;
+        System.out.println("------------------------");
+        System.out.println(" Secuencia De Fibonacci ");
+        System.out.println("------------------------");
+        System.out.print("Ingrese el limite de la sucuencia: "); limite = input.nextInt();
+        input.nextLine();
+        System.out.println("------------------------");
+        System.out.print("[ ");
+        for (int i = 0; i < limite; i++) {
+            System.out.print(factorialR(i) + " ");
+        }
+        System.out.print(" ]");
+    }
+
 }
