b6629e2233faf04046f617e762e864d467ee0590
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-16 12:08:04 -0500

    Agrega clase EPares con método para sumar números pares
    Se implementa la clase EPares que permite al usuario ingresar un número N y calcula la suma de los primeros N números pares. Incluye un método recursivo sumaPar para realizar el cálculo.

---
+++ src/Ejercicios/EPares.java
@@ -0,0 +1,34 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class EPares {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = 0;
+        int resul = 0;
+        System.out.println("---------------------");
+        System.out.println("   Suma De N Pares   ");
+        System.out.println("---------------------");
+        System.out.println("Ingrese la cantidad N a sumar: ");
+        n = input.nextInt();
+        input.nextLine();
+        resul = sumaPar(n);
+        System.out.println("El resultado es: " + resul);
+        input.close();
+
+    }
+
+    public static int sumaPar(int n){
+
+        if(n <= 0){ // si n llega a ser menor igual a 0
+            return 0; // retorna Cero
+        }
+
+        if(n % 2 != 0){ // si el residuo de n es distinto de 0
+            n--; // Decrementa
+        }
+
+        return n + sumaPar(n-2);
+    }
+}
