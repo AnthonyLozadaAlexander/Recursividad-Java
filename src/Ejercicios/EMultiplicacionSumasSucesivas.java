be2ed91b5db75f6749cb1ebf50fac6f2ff98f274
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-17 11:54:25 -0500

    Agrega método main en EMultiplicacionSumasSucesivas para multiplicar números usando suma sucesiva
    Implementa la lógica para solicitar dos números al usuario y calcular su producto mediante un método recursivo. Mejora la interacción del usuario al incluir mensajes claros y cierra el escáner al final del proceso.

--- src/Ejercicios/EMultiplicacionSumasSucesivas.java
+++ src/Ejercicios/EMultiplicacionSumasSucesivas.java
@@ -1,7 +1,33 @@
 ﻿package Ejercicios;
 
+import java.util.Scanner;
+
 public class EMultiplicacionSumasSucesivas {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        int resultado = 0;
+        System.out.println("Ingrese dos numeros para multiplicar");
+        System.out.println("Ingrese A: ");
+        a = input.nextInt();
+        input.nextLine();
 
+        System.out.println("Ingrese B: ");
+        b = input.nextInt();
+        input.nextLine();
+
+        resultado = sumaS(a, b);
+        System.out.println("El resultado de multiplicar: ("+ a + ")(" + b + ") = " + resultado);
+
+        input.close();
+    }
+
+    public static int sumaS(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+
+        return a + sumaS(a, b - 1);
     }
 }
