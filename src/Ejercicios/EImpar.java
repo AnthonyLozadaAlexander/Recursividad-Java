2d82ccf570b1d91c447ee36113a125cbb7d30f1e
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-17 11:34:31 -0500

    Corrige el método main en EImpar para que sea público y mejora la legibilidad del código. Ajusta el método Enumerar para incluir espacios en la salida y corrige un error de sintaxis al cerrar el escáner.

--- src/Ejercicios/EImpar.java
+++ src/Ejercicios/EImpar.java
@@ -1,7 +1,30 @@
 package Ejercicios;
 
-public class EImpar {
-    static void main(String[] args) {
+import java.util.Scanner;
 
+public class EImpar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = 0;
+        int resultado = 0;
+        System.out.println("Ingrese Hasta Que Numero Impar Desea Sumar: ");
+        n = input.nextInt();
+        input.nextLine();
+        resultado = sumaImpar(n);
+        System.out.println("La suma de los numeros impares hasta " + n + " es: " + resultado);
+        input.close();
+    }
+
+    public static int sumaImpar(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            n = n - 1; // n--
+            return sumaImpar(n);
+        } else {
+            return n + sumaImpar(n - 1);
+        }
     }
 }
--- src/Ejercicios/Repaso/Enumerar.java
+++ src/Ejercicios/Repaso/Enumerar.java
@@ -12,16 +12,18 @@ public class Enumerar {
         num = leer.nextInt();
         leer.nextLine();
         Enumerar(num);
-        leer.close()
+        leer.close();
 
     }
 
     public static void Enumerar(int num) {
         System.out.print("[ ");
         for (int i = 0; i <= num; i++) {
-            System.out.print(i + "Maury");
+            if (i < num) { // i <= num - 1
+                System.out.print(i + " Maury, ");
+            }
             if (i == num) {
-                System.out.print(i);
+                System.out.print(i + " Maury ");
             }
         }
         System.out.print(" ]");
