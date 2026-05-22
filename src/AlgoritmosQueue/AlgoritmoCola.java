69be0916296dd79a16a30e651a0711fbb35388ac
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-21 21:31:30 -0500

    Agrega clase AlgoritmoCola con métodos para invertir cola
    Se implementa la clase AlgoritmoCola que incluye el método
    invertirCola para invertir el orden de los elementos en una
    cola. Este método utiliza recursión y maneja la excepción
    ColaVacia. Se deja un espacio para implementar el método
    ultimoAPrimero en futuras actualizaciones.

---
+++ src/AlgoritmosQueue/AlgoritmoCola.java
@@ -0,0 +1,22 @@
+﻿package AlgoritmosQueue;
+
+import Queue.*;
+
+public class AlgoritmoCola<T> {
+
+    public static <T> void invertirCola(Cola<T> cola) throws ColaVacia {
+        Cola<T> aux;
+        aux = cola;
+
+        if(!cola.colaVacia()){
+            T elem = cola.desencolar();
+            invertirCola(aux);
+            aux.encolar(elem);
+        }
+    }
+
+    public static <T> void ultimoAPrimero(Cola<T> cola){
+
+    }
+
+}
