91f22cd6c7015a10f51cb3dc7b988e4b2d9753b2
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-24 15:06:47 -0500

    Modifica la cola de prueba eliminando elementos innecesarios y ajustando el orden de inserción. Se mantiene la funcionalidad de impresión y se asegura que el método ultimoAlFrente se ejecute correctamente.

--- src/PruebasQueue/PruebaQueue01.java
+++ src/PruebasQueue/PruebaQueue01.java
@@ -7,15 +7,12 @@ import AlgoritmosQueue.*;
 public class PruebaQueue01 {
     static void main(String[] args) {
         TadCola<Integer> cola = new TadCola<>("Cola Integers");
-        cola.encolar(4);
-        cola.encolar(7);
         cola.encolar(1);
-        cola.encolar(8);
         cola.encolar(2);
-        cola.encolar(5);
+        cola.encolar(3);
+        cola.encolar(4);
         System.out.println("Cola Original");
         cola.imprimirCola();
-
 
 
         try {
@@ -26,13 +23,17 @@ public class PruebaQueue01 {
             // System.out.println("Llevar Ultimo Elemento De La Cola Al Inicio");
             // AlgoritmoCola.ultimoAPrimero(cola);
 
-            int tamanio = AlgoritmoCola.contarElementsR(cola, 0);
-            System.out.println("La cantidad de la cola es de: " + tamanio);
+            //int tamanio = AlgoritmoCola.contarElementsR(cola, 0);
+            //System.out.println("La cantidad de la cola es de: " + tamanio);
+
             System.out.println("Cola Invertida");
             // cola.invertirCola();
             cola.imprimirCola();
-            int countPares = AlgoritmoCola.contarParesCola(cola);
-            System.out.println("Hay " + countPares + " numeros pares en la Cola");
+            // int countPares = AlgoritmoCola.contarParesCola(cola);
+            // System.out.println("Hay " + countPares + " numeros pares en la Cola");
+            System.out.println("Primer Elemento Al Ultimo");
+            AlgoritmoCola.ultimoAlFrente(cola);
+            cola.imprimirCola();
 
 
         } catch (ColaVacia e) {
