6021631aeb77bddbe30339272da14a18b2adadac
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-24 15:32:03 -0500

    Agrega clase PruebaStack02 para probar operaciones en la pila
    Se implementa la clase PruebaStack02 que permite apilar elementos en una instancia de TadPila y probar el método insertarAlFondo. Se incluye manejo de excepciones para el caso de pila vacía al intentar insertar un elemento.

---
+++ src/PruebasStack/PruebaStack02.java
@@ -0,0 +1,24 @@
+package PruebasStack;
+
+import Stack.*;
+import AlgoritmosStack.*;
+
+public class PruebaStack02 {
+    static void main(String[] args) {
+        TadPila<Integer> miPila = new TadPila<>("Pila Integers");
+        miPila.apilar(3);
+        miPila.apilar(2);
+        miPila.apilar(1);
+
+        System.out.println("Pila Actual");
+        miPila.imprimirPila();
+        try {
+            AlgoritmosPila.insertarAlFondo(miPila, 4);
+            miPila.imprimirPila();
+        } catch (PilaVacia e) {
+
+        }
+
+    }
+
+}
