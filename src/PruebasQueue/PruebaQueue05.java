2023d1ff9c572c513efdb2453a98f5470c496a39
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-25 15:49:23 -0500

    Agrega la clase PruebaQueue05 para probar la funcionalidad de la cola, incluyendo la inserción de elementos y la ejecución del método para intercambiar elementos pares.

---
+++ src/PruebasQueue/PruebaQueue05.java
@@ -0,0 +1,26 @@
+package PruebasQueue;
+
+import AlgoritmosQueue.AlgoritmoCola;
+import Queue.ColaVacia;
+import Queue.TadCola;
+
+public class PruebaQueue05 {
+    public static void main(String[] args) {
+        TadCola<Integer> cola = new TadCola<>("Cola Integers");
+        cola.encolar(5);
+        cola.encolar(6);
+        cola.encolar(7);
+        cola.encolar(8);
+        cola.encolar(1);
+        cola.imprimirCola();
+
+        try {
+            AlgoritmoCola.intercambiarParesA(cola);
+            cola.imprimirCola();
+
+        } catch (ColaVacia e) {
+
+        }
+    }
+
+}
