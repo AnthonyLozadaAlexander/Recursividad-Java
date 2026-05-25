122eeeb7873793edc01a80028c792d4df7d08b59
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-25 15:50:02 -0500

    Agrega la clase PruebaQueue07 para probar la funcionalidad de la cola, incluyendo la inserción de elementos y la ejecución del método para intercambiar la mitad de la cola.

---
+++ src/PruebasQueue/PruebaQueue07.java
@@ -0,0 +1,26 @@
+package PruebasQueue;
+
+import AlgoritmosQueue.AlgoritmoCola;
+import Queue.ColaVacia;
+import Queue.TadCola;
+
+public class PruebaQueue07 {
+    public static void main(String[] args) {
+        TadCola<Integer> cola = new TadCola<>("Cola Integers");
+        cola.encolar(2);
+        cola.encolar(3);
+        cola.encolar(5);
+        cola.encolar(6);
+        cola.encolar(9);
+        cola.encolar(1);
+        cola.imprimirCola();
+
+        try {
+            
+            AlgoritmoCola.intercambiarMitad(cola);
+            cola.imprimirCola();
+        } catch (ColaVacia e) {
+
+        }
+    }
+}
