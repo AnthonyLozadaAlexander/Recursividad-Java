65f687f5960488800bd802b319aa7fa2f1c53863
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-26 18:54:13 -0500

    Agrega la clase 'PruebaStack08' para probar el método 'actualizarEnPosicion' de la clase 'AlgoritmosPila', permitiendo verificar la funcionalidad de actualización de un elemento en la pila.

---
+++ src/PruebasStack/PruebaStack08.java
@@ -0,0 +1,26 @@
+package PruebasStack;
+
+import AlgoritmosStack.AlgoritmosPila;
+import Stack.PilaVacia;
+import Stack.TadPila;
+
+public class PruebaStack08 {
+    public static void main(String[] args) throws PilaVacia {
+        TadPila<String> miPila = new TadPila<>("Pila Integers");
+        miPila.apilar("C");
+        miPila.apilar("B");
+        miPila.apilar("A");
+        // cima
+
+        System.out.println("Pila Actual");
+        miPila.imprimirPila();
+        try {
+            System.out.println("Actualizar Elemento ");
+            AlgoritmosPila.actualizarEnPosicion(miPila, 2, "W");
+            miPila.imprimirPila();
+        } catch (PilaVacia e) {
+
+        }
+    }
+
+}
