2a94c7207bc0b8f050fc44c9c8e7bd63bc03cadc
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-29 23:44:59 -0500

    Mejora el formato del código en la clase 'EAEuclides' para mayor claridad y legibilidad.

--- src/EjerciciosNormales/EAEuclides.java
+++ src/EjerciciosNormales/EAEuclides.java
@@ -1,14 +1,15 @@
 package EjerciciosNormales;
 
-class EAEuclides{
+class EAEuclides {
     static void main(String[] args) {
-        int a = 12; int b =18;
+        int a = 12;
+        int b = 18;
         int res = calcularMCD(a, b);
         System.out.println("MCD de " + a + " % " + b + " = " + res);
     }
 
-    static int calcularMCD(int a, int b){
-        if(b == 0){
+    static int calcularMCD(int a, int b) {
+        if (b == 0) {
             return Math.abs(a);
         }
         return calcularMCD(b, a % b);
