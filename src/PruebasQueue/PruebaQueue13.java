393086d7d402895fe4326c03ba873665958ea96b
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-31 03:19:29 -0500

    Agrega clase de prueba para la cola y método de inserción
    Se crea la clase 'PruebaQueue13' para probar la funcionalidad de la cola, incluyendo la inserción de un elemento delante de otro. Se implementa un ejemplo de uso que encola varios enteros y muestra el estado de la cola antes y después de la inserción.

---
+++ src/PruebasQueue/PruebaQueue13.java
@@ -0,0 +1,28 @@
+package PruebasQueue;
+
+import AlgoritmosQueue.AlgoritmoCola;
+import Queue.ColaVacia;
+import Queue.TadCola;
+
+public class PruebaQueue13 {
+   public static void main(String[] args) {
+        TadCola<Integer> cola = new TadCola<>("Cola Integers");
+        cola.encolar(4);
+        cola.encolar(7);
+        cola.encolar(2);
+        cola.encolar(5);
+        cola.encolar(1);
+        cola.imprimirCola();
+
+        try {
+
+            AlgoritmoCola.insertarDelanteDeB(cola, 8, 2);
+            cola.imprimirCola();
+
+
+        } catch (ColaVacia e) {
+
+        }
+
+    }
+}
