b5316230be9a837272521cb9f63acf83ee4e0975
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-02 01:49:59 -0500

    Agrega pruebas para la clase TadLista e inserción de elementos
    Se implementa un método main en la clase pruebaList01 que crea una instancia de TadLista y utiliza métodos de AlgoritmosListas para insertar elementos al final de la lista. Esto permite verificar el correcto funcionamiento de la clase TadLista y su método de impresión.

--- src/PruebasList/pruebaList01.java
+++ src/PruebasList/pruebaList01.java
@@ -1,7 +1,14 @@
-﻿package PruebasList;
+package PruebasList;
+
+import AlgoritmosListas.*;
+import List.TadLista;
 
 public class pruebaList01 {
     public static void main(String[] args) {
-
+        TadLista<Integer> miLista = new TadLista<>();
+        AlgoritmosListas.insertarAlFinal(miLista, 1);
+        AlgoritmosListas.insertarAlFinal(miLista, 2);
+        AlgoritmosListas.insertarAlFinal(miLista, 3);
+        miLista.imprimirLista();
     }
 }
