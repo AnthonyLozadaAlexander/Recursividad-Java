bd712704444b5bf8efcf78f7702f884f4e5149c3
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-03 05:37:54 -0500

    Agrega métodos para crear y manipular pilas desde archivos
    Implementa la función crearPila para leer elementos de un archivo y apilarlos.
    Incluye métodos para intercambiar el elemento superior con el fondo de la pila,
    así como métodos auxiliares para obtener y reemplazar el fondo de la pila.

--- src/SerializacionExcersises/practica01.java
+++ src/SerializacionExcersises/practica01.java
@@ -9,4 +9,72 @@ public class practica01 {
     public static void main(String[] args) {
 
     }
+
+    public static  <T> Pila<T> crearPila(String archivo){
+       Pila<T> pila = new TadPila<>("Pila");
+
+       if(archivo != null && !archivo.isEmpty()){
+           Lectura<T> lector = new Lectura<>(archivo);
+           try{
+               lector.abrir();
+               crearPilaR(lector, pila);
+               lector.cerrar();
+           }catch(Exception e){
+               System.err.println(e.getMessage());
+           }
+       }
+
+        return pila;
+    }
+
+    private static <T> void crearPilaR(Lectura<T> lector, Pila<T> pila){
+        try{
+            T objeto = lector.leer();
+            if(objeto != null){ // si el objeto existe
+                crearPilaR(lector, pila);
+                pila.apilar(objeto);
+            }
+        }catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static <T> void intercambiar(Pila<T> pila){
+        try{
+            if(pila != null && !pila.pilaVacia() && pila.getTamanio() >  1){
+                T cima = pila.desapilar();
+                T fondo = obtenerFondoR(pila);
+                remplazarFondoR(pila, cima);
+                pila.apilar(fondo);
+            }
+        }catch(PilaVacia e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static <T> T obtenerFondoR(Pila<T> pila) throws PilaVacia{
+        T guardar = pila.desapilar();
+        T fondo;
+
+        if(pila.pilaVacia()){
+            fondo = guardar; // fondo
+            pila.apilar(guardar);
+        }else{
+            fondo = obtenerFondoR((pila));
+            pila.apilar(fondo);
+        }
+        return fondo;
+    }
+
+    private static <T> void remplazarFondoR(Pila<T> pila, T nuevoValor) throws PilaVacia {
+        T guardar = pila.desapilar();
+        if(pila.pilaVacia()){
+            pila.apilar(nuevoValor);
+        }else{
+            remplazarFondoR(pila, nuevoValor);
+            pila.apilar(guardar);
+        }
+    }
+
+
 }
