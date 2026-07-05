962d3ab72edd354143ddfe1fd169e83994f84029
zDefcon21 <antholozadarequelme@gmail.com>
2026-07-05 03:16:25 -0500

    Agrega clase de prueba para calcular la altura de un árbol

---
+++ src/PruebasArboles/pruebaArbol03.java
@@ -0,0 +1,26 @@
+package PruebasArboles;
+
+import AlgoritmosArboles.AlgoritmosArbolesBinario;
+import Arboles.Arbol;
+
+public class pruebaArbol03 {
+    public static void main() {
+        // Nodos
+        Arbol<Integer> a1 = new Arbol<>(1);
+        Arbol<Integer> a4 = new Arbol<>(4);
+        Arbol<Integer> a6 = new Arbol<>(6);
+        Arbol<Integer> a9 = new Arbol<>(9);
+
+        // Raices
+        Arbol<Integer> a3 = new Arbol<>();
+        Arbol<Integer> a7 = new Arbol<>();
+        Arbol<Integer> a5 = new Arbol<>();
+
+        Arbol.juntar(a3, 3, a1, a4);
+        Arbol.juntar(a7, 7, a6, a9);
+        Arbol.juntar(a5, 5, a3, a7);
+
+        int count = AlgoritmosArbolesBinario.altura(a5);
+        System.out.println(count);
+    }
+}
