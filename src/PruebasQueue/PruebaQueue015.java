bedb319801373048a1b17c6d0851ea5cf36cbb79
zDefcon21 <antholozadarequelme@gmail.com>
2026-07-16 17:26:31 -0500

    Agrega clase de prueba para la función de eliminar la primera ocurrencia en la cola
    Se crea la clase PruebaQueue015 para probar la funcionalidad de eliminación de la primera ocurrencia en la cola. Esta clase encola varios elementos y verifica el estado de la cola después de realizar la operación de eliminación.

---
+++ src/PruebasQueue/PruebaQueue015.java
@@ -0,0 +1,26 @@
+package PruebasQueue;
+
+import AlgoritmosQueue.AlgoritmoCola;
+import Queue.ColaVacia;
+import Queue.TadCola;
+
+public class PruebaQueue015 {
+    public static void main(String[] args) {
+        TadCola<Integer> cola = new TadCola<>("Cola Integers");
+        cola.encolar(1);
+        cola.encolar(2);
+        cola.encolar(3);
+        cola.encolar(4);
+
+        cola.imprimirCola();
+
+        try {
+
+            AlgoritmoCola.eliminarPrimeraOcurrencia(cola,  3);
+            cola.imprimirCola();
+
+        } catch (ColaVacia e) {
+
+        }
+    }
+}
